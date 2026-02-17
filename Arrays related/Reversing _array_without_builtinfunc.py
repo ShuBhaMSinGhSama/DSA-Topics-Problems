36b3e02a404a656828f6e310d9310e9a2052a3dd
Shubham Singh <shubhamsingh090105@gmail.com>
2026-02-17 15:26:23 +0530

    Update Reversing _array_without_builtinfunc.py

--- Arrays related/Reversing _array_without_builtinfunc.py
+++ Arrays related/Reversing _array_without_builtinfunc.py
@@ -1,1 +1,13 @@
+a=[3,5,2,6,7,5,8,1,4]
 
+def reversing(a):
+    i=0
+    j=len(a)-1
+    
+    while i<j:
+        a[i],a[j] = a[j],a[i]
+
+        i+=1
+        j-=1
+    return(a)
+print(reversing(a))
